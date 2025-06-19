e6380c0128d4c1baeb065375a9a9e2e29931a88e
Mohammed Sinan A K <sinan72727@gmail.com>
2025-06-19 17:25:23 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
-import db_helper
-import general_helper
+from . import db_helper
+from . import general_helper
 
 app=FastAPI()
 
