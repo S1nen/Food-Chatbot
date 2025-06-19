f77babed30d86f1a39516b3be3e3d966b98bb208
s1nen <sinan72727@gmail.com>
2025-06-19 14:21:26 +0530

    Project completed end-to-end

--- backend/general_helper.py
+++ backend/general_helper.py
@@ -5,9 +5,14 @@ def get_session_id(session_str:str):
     if match:
         extracted_string=match.group(1)
         return extracted_string
+    return ""
+
+
+
 
 def get_values_from_food_dict(food_dict:dict):
     return ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+
 
 if __name__=="__main__":
     print(get_values_from_food_dict({"Pizza":3,"mango lassi":2}))
