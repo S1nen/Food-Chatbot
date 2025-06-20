d906deda7a7e3a64576a2dd8057e62d12b98864a
Mohammed Sinan A K <sinan72727@gmail.com>
2025-06-20 15:58:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -184,3 +184,10 @@ def cancel_order(parameters:dict,order_id:int):
     return JSONResponse(content={
         "fulfillmentText":fulfillment_text
     })
+
+
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000
